fa75d757919020a4542c7cffe6de91654fd9095d
Rodrigo Emanuel <drigosantos@gmail.com>
2022-10-12 09:34:04 -0300

    Add Database Postgres

---
+++ server/prisma/migrations/20221012122007_create_table_ads/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Ad" (
+    "id" TEXT NOT NULL,
+    "gameId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "yearsPlaying" INTEGER NOT NULL,
+    "discord" TEXT NOT NULL,
+    "hourStart" INTEGER NOT NULL,
+    "HourEnd" INTEGER NOT NULL,
+    "useVoiceChannel" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Ad_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Ad" ADD CONSTRAINT "Ad_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
